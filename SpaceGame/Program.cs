90298f2cbefd4d74c2b2e850a898a1e2c6140c13
BullEKorV <elias.wasielewski@elev.ga.ntig.se>
2021-03-01 20:53:33 +0100

    aw

---
+++ SpaceGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpaceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("f f!");
+        }
+    }
+}
+// ff
