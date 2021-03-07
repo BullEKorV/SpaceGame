d7fe979985e3181d8902b2f41a65952bf929b037
BullEKorV <elias.wasielewski@elev.ga.ntig.se>
2021-03-08 00:13:27 +0100

    Yeahhh

--- SpaceGame/Bullet.cs
+++ SpaceGame/Bullet.cs
@@ -7,14 +7,23 @@ class BulletScript
     {
         foreach (Bullet bullet in bullets)
         {
-            // bullet.rotation = Program.LookAt(bullet.x, -bullet.y, playerShip.x, -playerShip.y);
+            bullet.velocity = 0;
+            // Delete bullet if too far away
+            if (Math.Abs(bullet.y - playerShip.y) > 1200 || Math.Abs(bullet.x - playerShip.x) > 1500)
+            {
+                bullets.Remove(bullet);
+                break;
+            }
+
+            // bullet.rotation = Program.LookAt(bullet.x, bullet.y, playerShip.x, playerShip.y);
+
             var newPos = Program.CalculatePosition(bullet.x, bullet.y, bullet.velocity, bullet.rotation);
             float xPos = newPos.x;
             float yPos = newPos.y;
 
             bullet.x = xPos;
             bullet.y = yPos;
-            Console.WriteLine(xPos + " " + yPos + " " + playerShip.x + " " + playerShip.y);
+            // Console.WriteLine(xPos + " " + yPos + " " + playerShip.x + " " + playerShip.y);
         }
         return bullets;
     }
@@ -24,7 +33,7 @@ class BulletScript
         float xPos = pos.x;
         float yPos = pos.y;
 
-        bullets.Add(new Bullet(xPos, yPos, rotation, 7, 0, "player"));
+        bullets.Add(new Bullet(xPos, yPos, rotation, 14, "player"));
 
         return bullets;
     }
@@ -37,14 +46,12 @@ class Bullet
     public float rotation;
     public float velocity;
     public string type;
-    public float timeSpawned;
-    public Bullet(float x, float y, float rotation, float velocity, float timeSpawned, string type)
+    public Bullet(float x, float y, float rotation, float velocity, string type)
     {
         this.rotation = rotation;
         this.x = x;
         this.y = y;
         this.velocity = velocity;
-        this.timeSpawned = timeSpawned;
         this.type = type;
     }
 }
--- SpaceGame/Program.cs
+++ SpaceGame/Program.cs
@@ -11,16 +11,20 @@ class Program
 
         List<Bullet> bullets = new List<Bullet>();
 
-        Raylib.InitWindow(1000, 1000, "SpaceGame");
+        Raylib.InitWindow(1900, 1000, "SpaceGame");
         Raylib.SetTargetFPS(120);
 
         Dictionary<String, Texture2D> Textures = LoadTextures(); // Game Textures
+
+        // Give width and height to playership
+        playerShip.width = Textures["PlayerShip"].width;
+        playerShip.height = Textures["PlayerShip"].height;
 
         bool gameActive = true;
         while (gameActive)
         {
             Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.WHITE);
+            Raylib.ClearBackground(Color.BLACK);
 
             // Control player and spawn bullets
             var playerControl = PlayerControl(playerShip, bullets);
@@ -31,15 +35,6 @@ class Program
 
             CheckCollision(bullets, playerShip);
 
-            // if (Raylib.IsKeyPressed(KeyboardKey.KEY_B))
-            // {
-            //     foreach (var bullet in bullets)
-            //     {
-            //         Console.WriteLine(bullet.y);
-            //     }
-            //     Console.WriteLine(playerShip.y);
-            // }
-
             RenderWorld(Textures, playerShip, bullets);
 
             // Console.WriteLine((int)playerShip.x + " " + (int)playerShip.y + " " + playerShip.rotation);
@@ -49,20 +44,32 @@ class Program
     }
     static void CheckCollision(List<Bullet> bullets, SpaceShip playerShip)
     {
+        double radians = (Math.PI / 180) * playerShip.rotation;
+
+        float p1X = (float)((playerShip.x - (playerShip.x + playerShip.width / 2)) * Math.Cos(radians) - (playerShip.y - (playerShip.y + playerShip.height / 2)) * Math.Sin(radians));
+        float p1Y = (float)((playerShip.x - (playerShip.x + playerShip.width / 2)) * Math.Sin(radians) + (playerShip.y - (playerShip.y + playerShip.height / 2)) * Math.Cos(radians));
+
+        float p2X = (float)((playerShip.x + playerShip.width - (playerShip.x + playerShip.width / 2)) * Math.Cos(radians) - (playerShip.y - (playerShip.y + playerShip.height / 2)) * Math.Sin(radians));
+        float p2Y = (float)((playerShip.x + playerShip.width - (playerShip.x + playerShip.width / 2)) * Math.Sin(radians) + (playerShip.y - (playerShip.y + playerShip.height / 2)) * Math.Cos(radians));
+
+        float p3X = (float)((playerShip.x - (playerShip.x + playerShip.width / 2)) * Math.Cos(radians) - (playerShip.y + playerShip.height - (playerShip.y + playerShip.height / 2)) * Math.Sin(radians));
+        float p3Y = (float)((playerShip.x - (playerShip.x + playerShip.width / 2)) * Math.Sin(radians) + (playerShip.y + playerShip.height - (playerShip.y + playerShip.height / 2)) * Math.Cos(radians));
+
+        float p4X = (float)((playerShip.x + playerShip.width - (playerShip.x + playerShip.width / 2)) * Math.Cos(radians) - (playerShip.y + playerShip.height - (playerShip.y + playerShip.height / 2)) * Math.Sin(radians));
+        float p4Y = (float)((playerShip.x + playerShip.width - (playerShip.x + playerShip.width / 2)) * Math.Sin(radians) + (playerShip.y + playerShip.height - (playerShip.y + playerShip.height / 2)) * Math.Cos(radians));
+
+
+        Console.WriteLine(p1X + " " + p1Y);
+
+        Raylib.DrawRectangle((int)p1X + Raylib.GetScreenWidth() / 2, (int)p1Y + Raylib.GetScreenHeight() / 2, 10, 10, Color.RED);
+        Raylib.DrawRectangle((int)p2X + Raylib.GetScreenWidth() / 2, (int)p2Y + Raylib.GetScreenHeight() / 2, 10, 10, Color.DARKGRAY);
+        Raylib.DrawRectangle((int)p3X + Raylib.GetScreenWidth() / 2, (int)p3Y + Raylib.GetScreenHeight() / 2, 10, 10, Color.ORANGE);
+        Raylib.DrawRectangle((int)p4X + Raylib.GetScreenWidth() / 2, (int)p4Y + Raylib.GetScreenHeight() / 2, 10, 10, Color.BLUE);
+
         foreach (var bullet in bullets)
         {
-            Rectangle obstacle = new Rectangle(bullet.x, bullet.y, 10, 10);
-            Rectangle player = new Rectangle(playerShip.x, playerShip.y, 140, 140);
 
-            if (Raylib.CheckCollisionRecs(obstacle, player))
-            {
-                // Console.WriteLine("Hej");
-                // var newPos = CalculatePosition(playerShip.x, playerShip.y, -playerShip.velocity, playerShip.rotation);
-                // playerShip.x = newPos.x;
-                // playerShip.y = newPos.y;
-            }
         }
-
     }
     static (SpaceShip, List<Bullet>) PlayerControl(SpaceShip playerShip, List<Bullet> bullets)
     {
@@ -78,13 +85,13 @@ class Program
         else if (playerShip.controllerType == "mouse")
         {
             // Make ship look at player
-            playerShip.rotation = LookAt(playerShip.x + Raylib.GetScreenWidth() / 2, playerShip.y + Raylib.GetScreenHeight() / 2, playerShip.x + Raylib.GetMouseX(), playerShip.y + Raylib.GetMouseY());
+            playerShip.rotation = LookAt(playerShip.x + Raylib.GetScreenWidth() / 2, playerShip.y - Raylib.GetScreenHeight() / 2, playerShip.x + Raylib.GetMouseX(), playerShip.y - Raylib.GetMouseY());
         }
 
         // Spawn bullet
         if ((Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) && playerShip.controllerType == "mouse") || (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && playerShip.controllerType == "arrow"))
         {
-            BulletScript.SpawnBullet(bullets, playerShip.x, playerShip.y, playerShip.rotation, 100);
+            BulletScript.SpawnBullet(bullets, playerShip.x, playerShip.y, playerShip.rotation, playerShip.height / 2);
         }
 
         // Calculate velocity
@@ -107,7 +114,7 @@ class Program
     }
     static public float LookAt(float x1, float y1, float x2, float y2)
     {
-        float deltaY = y2 - y1; // Calculate Delta y
+        float deltaY = y1 - y2; // Calculate Delta y
 
         float deltaX = x2 - x1; // Calculate delta x
 
@@ -147,26 +154,12 @@ class Program
         Raylib.DrawRectangle((int)(-playerShip.x - 50f), (int)(playerShip.y - 50f), 100, 100, Color.GREEN);
 
         // Draw player
-        // float sizeMultiplier = 0.7f;
-
-        // int shipWidth = Textures["PlayerShip"].width;
-        // int shipheight = Textures["PlayerShip"].height;
-
-        // Rectangle sourceRec = new Rectangle(0.0f, 0.0f, (float)shipWidth, (float)shipheight);
-
-        // Rectangle destRec = new Rectangle(Raylib.GetScreenWidth() / 2.0f, Raylib.GetScreenHeight() / 2.0f, shipWidth * sizeMultiplier, shipheight * sizeMultiplier);
-
-        // Vector2 origin = new Vector2((float)shipWidth * sizeMultiplier * 0.5f, (float)shipheight * sizeMultiplier * 0.5f);
-
-        // Raylib.DrawTexturePro(Textures["PlayerShip"], sourceRec, destRec, origin, playerShip.rotation, Color.WHITE);
-
         DrawObjectRotation(Textures["PlayerShip"], 0, 0, playerShip.rotation);
 
         // Draw bullets
         foreach (var bullet in bullets)
         {
-            DrawObjectRotation(Textures["PlayerShip"], (int)bullet.x - (int)playerShip.x, -(int)bullet.y + (int)playerShip.y, bullet.rotation);
-            // Raylib.DrawRectanglePro(new Rectangle((int)bullet.x - (int)playerShip.x + Raylib.GetScreenWidth() / 2, -(int)bullet.y + (int)playerShip.y + Raylib.GetScreenHeight() / 2, 10, 10), new Vector2(0, 0), bullet.rotation, Color.BLACK);
+            DrawObjectRotation(Textures["Laser"], (int)bullet.x - (int)playerShip.x, -(int)bullet.y + (int)playerShip.y, bullet.rotation);
         }
     }
     static void DrawObjectRotation(Texture2D texture, float x, float y, float rotation)
@@ -185,7 +178,8 @@ class Program
     static Dictionary<String, Texture2D> LoadTextures() // Load Textures
     {
         Dictionary<String, Texture2D> Textures = new Dictionary<string, Texture2D>();
-        Textures.Add("PlayerShip", Raylib.LoadTexture("Textures/player.png")); // Player ship
+        Textures.Add("PlayerShip", Raylib.LoadTexture("Textures/PlayerShip.png")); // Player ship
+        Textures.Add("Laser", Raylib.LoadTexture("Textures/Laser.png")); // Player ship
 
         return Textures;
     }
@@ -194,6 +188,8 @@ class SpaceShip
 {
     public float x;
     public float y;
+    public int width;
+    public int height;
     public float velocity;
     public float rotationVelocity;
     public float rotation;
@@ -204,8 +200,6 @@ class SpaceShip
         this.x = x;
         this.y = y;
         this.rotation = rotation;
-        // this.velocity = velocity;
-        // this.rotationVelocity = rotationVelocity;
         this.health = health;
         this.controllerType = controllerType;
     }
